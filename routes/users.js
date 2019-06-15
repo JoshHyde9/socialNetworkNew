3b26576a2fadaf9643c796aae120738ceefb9bb0
Josh Hyde <joshhyde@pop-os.localdomain>
2019-06-15 13:00:04 +1000

    Started partials

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,33 @@ router.get("/register", (req, res) => {
   res.render("register", { title: "Register an account" });
 });
 
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in al fields" });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: "Password must be at least 6 characters long" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send("success");
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -27,18 +27,20 @@ app.engine(
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+app.use(express.urlencoded({ extended: false }));
+
 // Routes
 app.use("/", require("./routes/showcase"));
 app.use("/users", require("./routes/users"));
 
-// Page error handling
-app.use((req, res) => {
-  res.status(404);
+// // Page error handling
+// app.use((req, res) => {
+//   res.status(404);
 
-  if (req.accepts("html")) {
-    return res.render("error", { title: "Page Not Found" });
-  }
-});
+//   if (req.accepts("html")) {
+//     return res.render("error", { title: "Page Not Found" });
+//   }
+// });
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
