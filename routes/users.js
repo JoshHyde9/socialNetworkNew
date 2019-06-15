ded62f6ac7499656b009e91a1193af2ea5e89b80
Josh Hyde <joshhyde@pop-os.localdomain>
2019-06-15 12:34:21 +1000

    Started form styling

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/login", (req, res) => {
-  res.send("Login");
+  res.render("login", { title: "Login to an existing account" });
 });
 
 router.get("/register", (req, res) => {
-  res.send("Register");
+  res.render("register", { title: "Register an account" });
 });
 
 module.exports = router;
