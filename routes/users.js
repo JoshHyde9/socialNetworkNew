dfa49bb3a6655c08c9fb4c27e429fccc3ec4a853
Josh Hyde <joshhyde@pop-os.localdomain>
2019-06-16 13:29:26 +1000

    Added redirects for login requests

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const passport = require("passport");
 
 const User = require("../models/Users");
 
@@ -83,6 +84,14 @@ router.post("/register", (req, res) => {
         console.log(err);
       });
   }
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/feed",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })(req, res, next);
 });
 
 module.exports = router;
