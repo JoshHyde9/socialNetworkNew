fe8b192b8b50ea4274c0cabbf7f32e4b05590039
Josh Hyde <joshhyde@pop-os.localdomain>
2019-06-22 19:03:57 +1000

    Posts post to db

--- routes/showcase.js
+++ routes/showcase.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { ensureAuthenticated } = require("../config/auth");
 
 const Profile = require("../models/Profiles");
+const Post = require("../models/Posts");
 
 router.get("/", (req, res) => {
   res.render("showcase", {
@@ -13,6 +14,23 @@ router.get("/", (req, res) => {
 
 router.get("/feed", ensureAuthenticated, (req, res) => {
   res.render("feed", { title: "Feed", name: req.user.name });
+});
+
+router.post("/feed", (req, res) => {
+  const { text } = req.body;
+
+  const userPost = new Post({
+    text
+  });
+
+  userPost
+    .save()
+    .then(user => {
+      req.flash("success_msg", "Account successfully created.");
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 router.get("/profile", ensureAuthenticated, (req, res) => {
